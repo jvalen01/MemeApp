235ee73b73668fc20765dde5f01f0d8096655b32
Jonatan Valen <jonatan.valen@student.uib.no>
2022-11-05 17:11:04 +0100

    Implemented html and flask basics

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import requests
+import json
+from threading import local
+
+tls = local()
+app = Flask(__name__)
+
+def getMeme():
+    url = "https://meme-api.herokuapp.com/gimme"
+    response = json.loads(requests.request("GET",url).text)
+    meme_large = response["preview"][-2]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
+
+@app.route("/")
+def index():
+    meme_pic, subreddit = getMeme()
+    return render_template("index.html", meme_pic=meme_pic, subreddit=subreddit)
